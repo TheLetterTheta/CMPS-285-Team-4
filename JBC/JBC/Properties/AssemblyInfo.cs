e782494d16408056dec00306840df71b7adc84e7
Phillip Jeremiah Weaver <jbw71699@gmail.com>
2017-09-17 16:11:37 -0500

    Fresh Start
    Starting fresh

--- JBC/JBC/Properties/AssemblyInfo.cs
+++ JBC/JBC/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using Xamarin.Forms.Xaml;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
@@ -25,5 +24,3 @@ using Xamarin.Forms.Xaml;
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
