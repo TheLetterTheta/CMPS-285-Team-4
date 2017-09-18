148f0b9529faa30cc3f4e63b0e8953a6955112c6
Zac Armand <zachary.armand@selu.edu>
2017-09-18 15:34:50 -0500

    Fresh Project
    Hopefully no errors...

--- JBC/JBC/obj/Debug/JBC.JBCPage.xaml.g.cs
+++ JBC/JBC/obj/Debug/JBC.JBCPage.xaml.g.cs
@@ -11,7 +11,7 @@
 namespace JBC {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("/Users/jbw716/Programming/285/CMPS-285-Team-4/JBC/JBC/JBCPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("/Users/willowoak716/Programming/CMPS-285-Team-4/JBC/JBC/JBCPage.xaml")]
     public partial class JBCPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
