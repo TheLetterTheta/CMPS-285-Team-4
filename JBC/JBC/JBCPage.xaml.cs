46625911f386e8a0b971f9b4fa754ab537959312
winterjadis <31132163+winterjadis@users.noreply.github.com>
2017-09-18 23:28:35 -0500

    New Home Pages and Their Navigation Functionality
    About Us (navigation) page and Location  (navigation) Page added. Simple buttons created with Click Event-Handler code to navigate between Home Page and {About Us ^ Location pages}.

---
+++ JBC/JBC/JBCPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace JBC
+{
+    public partial class JBCPage : TabbedPage
+    {
+        public JBCPage()
+        {
+            InitializeComponent();
+
+            //codebehind for making home page a navigation page...
+
+            //var navigationPage = new NavigationPage(new Home());
+            //navigationPage.Title = "Home";
+            //Children.Add(new Notes());
+            //Children.Add(new News());
+            //Children.Add(new Videos());
+        }
+    }
+}
