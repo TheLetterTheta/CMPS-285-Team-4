dfd66bd5baaa402f99576a1bc49f6a9e9ec03003
Zac Armand <zachary.armand@selu.edu>
2017-09-19 19:39:35 -0500

    Updated About Us Page
    Added the church's "About Us" statement to the About Us Page.

--- JBC/JBC/Home Pages/AboutUs.xaml.cs
+++ JBC/JBC/Home Pages/AboutUs.xaml.cs
@@ -15,6 +15,7 @@ namespace JBC
         public AboutUs()
         {
             InitializeComponent();
+            AboutLabel.Text = "\tAt Jerusalem Baptist Church, it’s our desire to be an open and welcoming place for community life and worship. We invite you to join us as we serve together for God’s glory. I believe that you’ll find Jerusalem Baptist Church to be a place where God is worshiped sincerely, Jesus Christ is preached triumphantly, His resurrection is celebrated joyously, the Gospel is shared unashamedly, and the Holy Spirit moves freely! I believe JBC is the place where you will find the help you need to overcome any physical obstacles, the healing to conquer any emotional hurts or struggles, and the hope to experience spiritual encouragement and a renewed purpose for living in Jesus Christ! Please join us for our weekly activities and special events. I’d love to share with you more about how you can become a part of the JBC family! I look forward to meeting you and pray God’s richest blessings upon you! \n\nIn Christ’s service, \n Dr. Phil Weaver, pastor";
         }
     }
 }
