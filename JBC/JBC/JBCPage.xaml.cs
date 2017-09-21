9479af2f15bcc4bd3120ddedd7aba767f8a9aea5
Phillip Jeremiah Weaver <jbw71699@gmail.com>
2017-09-20 20:41:34 -0500

    Changed platform specific navigation bar colors

--- JBC/JBC/JBCPage.xaml.cs
+++ JBC/JBC/JBCPage.xaml.cs
@@ -9,12 +9,16 @@ namespace JBC
             InitializeComponent();
 
             //codebehind for making home page a navigation page...
-            
+
             // var navigationPage = new NavigationPage(new Home());
             // navigationPage.Title = "Home";
             //Children.Add(new Notes());
             //Children.Add(new News());
             //Children.Add(new Videos());
+            if(Device.RuntimePlatform == Device.iOS)
+                BarBackgroundColor = Color.White;
+            else
+                BarBackgroundColor = Color.FromHex("#990000");
         }
     }
 }
