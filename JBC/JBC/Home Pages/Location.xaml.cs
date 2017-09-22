a289779e9c27ddfbd87a7d1f3ea8491c66d4ad52
Phillip Jeremiah Weaver <jbw71699@gmail.com>
2017-09-22 16:53:49 -0500

    Add Map and Pin To Location Page
    Added map and pin to the location page and linked the pin to the appropriate maps applications.

--- JBC/JBC/Home Pages/Location.xaml.cs
+++ JBC/JBC/Home Pages/Location.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
 
 namespace JBC
 {
@@ -15,6 +16,40 @@ namespace JBC
         public Location()
         {
             InitializeComponent();
+
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
+                        new Position(30.45, -90.54),
+                        Distance.FromMiles(0.5)));
+
+			// Latitude, Longitude of the church
+			var church = new Position(30.45, -90.54);
+
+            //Add pin at the church's latitude and longitude
+			var pin = new Pin
+			{
+				Type = PinType.Place,
+				Position = church,
+				Label = "Jerusalem Baptist Church",
+				Address = "11131 Jerusalem Baptist Church Rd. Hammond, LA 70403"
+			};
+
+			pin.Clicked += (sender, args) => {
+
+                if (Device.RuntimePlatform == Device.iOS)
+				{
+					// opens Apple Maps app directly
+					Device.OpenUri(new Uri("http://maps.apple.com/?q=11131+Jerusalem+Baptist+Church+Rd.+Hammond,+LA+70403"));
+				}
+                else if (Device.RuntimePlatform == Device.Android)
+				{
+					// opens Google Maps app directly
+					Device.OpenUri(new Uri("geo:0,0?q=11131+Jerusalem+Baptist+Church+Rd.+Hammond,+LA+70403"));
+
+				}
+
+			};
+
+			MyMap.Pins.Add(pin);
         }
 
         //async void Location_Clicked(object sender, EventArgs e)
