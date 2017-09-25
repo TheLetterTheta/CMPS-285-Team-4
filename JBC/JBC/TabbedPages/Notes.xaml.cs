214ef87c33f595b49dd51aafb0f6e7d558d1884b
Phillip Jeremiah Weaver <jbw71699@gmail.com>
2017-09-24 20:42:47 -0500

    Add new page for opening notes file functionality
    Added the functionality where when the user selects to view a notes file, the app opens a new page to show the file.

--- JBC/JBC/TabbedPages/Notes.xaml.cs
+++ JBC/JBC/TabbedPages/Notes.xaml.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 
+using JBC.TabbedPages;
+
 namespace JBC
 {
     public partial class Notes : ContentPage
     {
         public Notes()
         {
-            InitializeComponent();
+			InitializeComponent();
+
+            //Lambda expression for handling navigating to a Notes page.
+            directoryView.Navigated += (s, e) => {
+
+                directoryView.Navigating += async (sender, eArg) => {
+
+                    //eArg.Cancel = true;
+                    //String searchUrlString = "http://jbcpc.org/sermon_notes/#search=";
+                    //String filesUrlString = "http://jbcpc.org/sermon_notes/#files";
+                    String ignoreUrl = "http://jbcpc.org/sermon_notes/#";
+                    String requireUrl = "http://jbcpc.org/sermon_notes/files/";
+                    //if (!(eArg.Url.StartsWith(searchUrlString)) && !(eArg.Url.StartsWith(filesUrlString)) && (eArg.Url != "http://jbcpc.org/sermon_notes/"))
+                    if (!(eArg.Url.StartsWith(ignoreUrl)) && (eArg.Url.StartsWith(requireUrl)) && (eArg.Url != "http://jbcpc.org/sermon_notes/"))
+                    {
+                        
+                        eArg.Cancel = true;
+                        var uri = new Uri(eArg.Url);
+                        await Navigation.PushAsync(new Notes_View(uri));
+
+                    }
+
+                };
+
+            };
         }
     }
 }
