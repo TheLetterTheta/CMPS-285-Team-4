05ef48eab55cb3038bfafbe7a6eb7222d187ecbe
akaProxy <paul.sherrod@selu.edu>
2017-10-09 08:11:23 -0500

    Added Facebook SDK to project
    Figured out how to authenticate our app to work with facebook SDK as well as added it to project
    
    Did this with Aimee

--- JBC/JBC/TabbedPages/News.xaml.cs
+++ JBC/JBC/TabbedPages/News.xaml.cs
@@ -10,6 +10,22 @@ namespace JBC
         public News()
         {
             InitializeComponent();
+
+            // GraphRequest request = GraphRequest.newGraphPathRequest(
+            // accessToken,
+            // "/jbcpumkincenter",
+            // new GraphRequest.Callback() {
+            // @Override
+            // public void onCompleted(GraphResponse response)
+            // {
+            // Insert your code here
+            // }
+            // });
+
+            // Bundle parameters = new Bundle();
+            // parameters.putString("fields", "posts");
+            // request.setParameters(parameters);
+            // request.executeAsync();
         }
     }
 }
