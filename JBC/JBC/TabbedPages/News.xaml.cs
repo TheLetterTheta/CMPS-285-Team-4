484ba95ce2fd66379857354ad1c9d44893f63d25
Jeremiah Weaver <jbw71699@gmail.com>
2017-10-18 20:17:17 -0500

    Implemented WebView Solutions for News and Videos Pages
    Used WebViews and HTML plug-ins to implement the news and videos pages.

--- JBC/JBC/TabbedPages/News.xaml.cs
+++ JBC/JBC/TabbedPages/News.xaml.cs
@@ -7,6 +7,14 @@ namespace JBC
         public News()
         {
             InitializeComponent();
+
+            var fb_html = new HtmlWebViewSource();
+
+            fb_html.Html = "<html><body><iframe src=\"https://www.powr.io/plugins/facebook-feed/view?unique_label=421d8127_1508346868&external_type=iframe\" width=\"100%\" height=\"2500\" frameborder=\"0\"></iframe></body></html>";
+
+            //FBFeed.Source = fb_html;
+
+            FBFeed.Source = "https://www.powr.io/plugins/facebook-feed/view?unique_label=421d8127_1508346868&external_type=iframe";
         }
     }
 }
